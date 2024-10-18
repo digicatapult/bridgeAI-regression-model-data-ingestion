a1aa18b7d8c7de719018f4234e3532fdaebb0eb4
Renjith <renjith.baby@digicatapult.org.uk>
2024-10-18 14:20:14 +0100

    update version increment and add unit tests for the same

--- tests/test_data_push.py
+++ tests/test_data_push.py
@@ -2,7 +2,7 @@
 
 from unittest.mock import MagicMock, patch
 
-from src.data_push import push_data
+from src.data_push import get_latest_tag, push_data
 
 
 @patch("src.data_push.get_authenticated_github_url")
@@ -65,3 +65,96 @@ def test_push_data(
     mock_git_add_files.assert_called_once_with(mock_repo, config)
     mock_git_commit.assert_called_once_with(mock_repo, config)
     mock_git_push.assert_called_once_with(mock_repo, config)
+
+
+def test_no_tags():
+    """Test when there are no tags in the repository."""
+    repo = MagicMock()
+    repo.tags = []
+    result = get_latest_tag(repo)
+    assert result == "data-v1.0.0"
+
+
+def test_valid_data_v_tag():
+    """Test when a valid data-v tag exists.
+
+    The version should be incremented.
+    """
+    repo = MagicMock()
+
+    # Create a mock tag object
+    tag = MagicMock()
+    tag.name = "data-v1.2.0"
+    tag.commit.committed_datetime = "2000-01-01"
+
+    # Mock repo.tags to return the mock tag
+    repo.tags = [tag]
+
+    result = get_latest_tag(repo)
+    assert result == "data-v1.3.0"
+
+
+def test_multiple_data_v_tags():
+    """Test when multiple valid data-v tags exist.
+
+    The latest one should be selected and incremented.
+    """
+    repo = MagicMock()
+
+    # Create multiple mock tag objects
+    tag1 = MagicMock()
+    tag1.name = "data-v1.1.0"
+    tag1.commit.committed_datetime = "2000-01-01"
+
+    tag2 = MagicMock()
+    tag2.name = "data-v1.2.0"
+    tag2.commit.committed_datetime = "2001-01-01"
+
+    # Mock repo.tags to return both tags
+    repo.tags = [tag1, tag2]
+
+    result = get_latest_tag(repo)
+    assert result == "data-v1.3.0"
+
+
+def test_non_data_v_tags():
+    """Test when non data-v tags exist.
+
+    All the non data-v tags should be ignored."""
+    repo = MagicMock()
+
+    # Create mock tag objects where one is unrelated
+    tag1 = MagicMock()
+    tag1.name = "v1.0.1"
+    tag1.commit.committed_datetime = "2002-01-01"
+
+    tag2 = MagicMock()
+    tag2.name = "data-v1.0.0"
+    tag2.commit.committed_datetime = "2001-01-01"
+
+    # Mock repo.tags to return both tags
+    repo.tags = [tag1, tag2]
+
+    result = get_latest_tag(repo)
+    assert result == "data-v1.1.0"
+
+
+def test_empty_data_v_tag():
+    """Test when no valid data-v tag exists.
+
+    Should return data-v1.0.0."""
+    repo = MagicMock()
+
+    # Mock repo.tags with tags that don't match the 'data-v' pattern
+    tag1 = MagicMock()
+    tag1.name = "v1.0.0"
+    tag1.commit.committed_datetime = "2000-01-01"
+
+    tag2 = MagicMock()
+    tag2.name = "v2.0.0"
+    tag2.commit.committed_datetime = "2001-01-01"
+
+    repo.tags = [tag1, tag2]
+
+    result = get_latest_tag(repo)
+    assert result == "data-v1.0.0"
